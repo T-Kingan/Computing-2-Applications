23fb521c8e459a5bfd564e3e21e752827cbe695c
T-Kingan <thomas.kingan20@imperial.ac.uk>
2021-05-23 16:03:18 +0100

    -

---
+++ Week 3 - More functional/web-app/connect_four.js
@@ -0,0 +1,4 @@
+new_board = () => board
+// takes no arguements and returns a new empty board
+
+red_move
--- Week 3 - More functional/web-app/tutorial.js
+++ Week 3 - More functional/web-app/tutorial.js
@@ -6,24 +6,24 @@ const tutorial = Object.create(null);
 
 const sum = function (a,b){
     return a + b;
-}
+};
 
 const factorial = function(x){
     if (x===0){
         return 1;
     }
     else{
-        return x * factorial(x-1)
+        return x * factorial(x-1);
     }
-}
+};
 
-// write function again as turnary? 
+// write function again as turnary?
 
 const factorial_simp = (x) => {
     return (x === 0) ? 1 : (x * factorial_simp(x-1))
 };
 
-// turnary in conditional variable assignment 
+// turnary in conditional variable assignment
 
 const fibonacci = function(n){
     if (n === 0){
@@ -33,10 +33,34 @@ const fibonacci = function(n){
     } else {
         return(fibonacci(n - 1) + fibonacci(n - 2));
     }
-}
+};
 // make one that could start at any point in the sequence
-const 
 
+const fibonacci_like = function(x, y){
+    return function(n){
+        if (n === 0){
+            return x;
+        } else if (n === 1) {
+            return y;
+        } else {
+            return(fibonacci_like(n - 1) + fibonacci_like(n - 2));
+        }
+    };
+}
+
+const fibonacci_new = function(n){
+    if (n === 0){
+        return 0;
+    } else if (n === 1){
+        return 1;
+    } else {
+        return(fibonacci(n - 1) + fibonacci(n - 2));
+    }
+};
+
+if (fibonacci_like(2308, 4261)(8) === 119485){
+    console.log("TRUE");
+}
 
 debugger;
 
